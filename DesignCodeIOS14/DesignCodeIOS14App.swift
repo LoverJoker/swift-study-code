65975e00e000762ed6c7373a6a0013de8b041d0a
JokerLiang <we@jokerliang.cn>
2020-10-04 10:19:58 +0800

    Initial Commit

---
+++ DesignCodeIOS14/DesignCodeIOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS14App.swift
+//  DesignCodeIOS14
+//
+//  Created by Joker Liang on 2020/10/4.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
